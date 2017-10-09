0bafcec2310ca170b29c7cfd0ce86324f133cf13
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-09 22:52:53 +0200

    Test caesar with key 7 and test string

---
+++ AffineCipher/Tests/CipherTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AffineCipher;
+using Xunit;
+
+namespace Test
+{
+    public class CipherTests
+    {
+        [Fact]
+        public void CaesarWithKey7ConvertsTestToAlza()
+        {
+            var input = "Test";
+            var output = Cipher.CipherCaesar(input, 7);
+
+            Assert.Equal("Alza", output);
+        }
+    }
+}
