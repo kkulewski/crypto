b97872deadd2e12cb37e899829eedefc6277aad4
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-09 22:14:32 +0200

    Add modular inverse existence tests

--- AffineCipher/Tests/ModularInverterTest.cs
+++ AffineCipher/Tests/ModularInverterTest.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using AffineCipher;
 using Xunit;
 
 namespace Tests
 {
     public class ModularInverterTest
     {
-        public void TestMethod1()
+        [Fact]
+        public void InversionOf7Mod26Exists()
         {
+            Assert.True(ModularInverter.GetInversionNaive(7, 26, out int _));
+        }
+
+        [Fact]
+        public void InversionOf4Mod26DoesNotExist()
+        {
+            Assert.False(ModularInverter.GetInversionNaive(4, 26, out int _));
         }
     }
 }
