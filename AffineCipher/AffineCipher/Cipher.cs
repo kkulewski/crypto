056bc133a958ee0d4a17e5e6a777d7a00d678274
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-09 22:44:19 +0200

    Add cipher with caesar algorithm

---
+++ AffineCipher/AffineCipher/Cipher.cs
@@ -0,0 +1,26 @@
+﻿namespace AffineCipher
+{
+    class Cipher
+    {
+        public string CipherCaesar(string input, int key)
+        {
+            var alphabetSize = 'z' - 'a' + 1;
+            var chars = input.ToCharArray();
+
+            for(int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] >= 'a' && chars[i] <= 'z')
+                {
+                    chars[i] = (char)((chars[i] - 'a' + key) % alphabetSize + 'a');
+                }
+
+                if (chars[i] >= 'A' && chars[i] <= 'Z')
+                {
+                    chars[i] = (char)((chars[i] - 'A' + key) % alphabetSize + 'A');
+                }
+            }
+
+            return chars.ToString();
+        }
+    }
+}
