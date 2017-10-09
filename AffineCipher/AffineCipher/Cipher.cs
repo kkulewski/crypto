686cac0b936c6555ae0a4aca827f4ebbc98733ee
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-10 00:35:35 +0200

    Add cipher abstract class

---
+++ AffineCipher/AffineCipher/Cipher.cs
@@ -0,0 +1,13 @@
+﻿namespace AffineCipher
+{
+    public abstract class Cipher
+    {
+        public const int AlphabetSize = 'z' - 'a' + 1;
+
+        public abstract string Encrypt(string input, int keyAddend, int keyMultiplier = 1);
+
+        public abstract string Decrypt(string input, int keyAddend, int keyMultiplier = 1);
+
+        public abstract int RunCryptoanalysisWithPlain(string plain, string encrypted);
+    }
+}
