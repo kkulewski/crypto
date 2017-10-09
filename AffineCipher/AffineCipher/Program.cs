b8ece68196a487a3b12b1dfcb55a0497259a769d
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-10 00:54:23 +0200

    Add flow control draft

--- AffineCipher/AffineCipher/Program.cs
+++ AffineCipher/AffineCipher/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AffineCipher
 {
@@ -10,6 +6,48 @@ namespace AffineCipher
     {
         static void Main(string[] args)
         {
+            if (args[0] == null)
+            {
+                Console.WriteLine("No cipher method selected!");
+                return;
+            }
+
+            if (args[1] == null)
+            {
+                Console.WriteLine("No action selected!");
+                return;
+            }
+
+            Cipher cipher = new CaesarCipher();
+            var cipherType = args[0];
+            var action = args[1];
+
+            switch (cipherType)
+            {
+                case "-c":
+                    cipher = new CaesarCipher();
+                    break;
+
+                case "-a":
+                    cipher = new AffineCipher();
+                    break;
+            }
+
+            switch (action)
+            {
+                case "-e":
+                    Console.WriteLine(cipher.Encrypt("Test", 1));
+                    break;
+
+                case "-d":
+                    Console.WriteLine(cipher.Decrypt("Uftu", 1));
+                    break;
+
+                case "-j":
+                    Console.WriteLine(cipher.RunCryptoanalysisWithPlain("Test", "Uftu"));
+                    break;
+            }
+
         }
     }
 }
