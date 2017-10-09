48c30fa88fd9be0b7efebcb821b52523954c5e05
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-09 21:56:43 +0200

    Add modular inverter with naive inversion algorithm

---
+++ AffineCipher/AffineCipher/ModularInverter.cs
@@ -0,0 +1,22 @@
+﻿namespace AffineCipher
+{
+    public static class ModularInverter
+    {
+        public static bool GetInversionNaive(int number, int modulo, out int inversion)
+        {
+            inversion = 0;
+
+            for (int counter = 0; counter < modulo; counter++)
+            {
+                int result = (counter * number) % modulo;
+                if (result == 1)
+                {
+                    inversion = counter;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
