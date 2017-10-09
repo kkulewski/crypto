204bb053ac829b6b4fd4429c2aae87ebcb5669fc
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-09 22:18:07 +0200

    Add modular inversion naive return value tests

--- AffineCipher/Tests/ModularInverterTest.cs
+++ AffineCipher/Tests/ModularInverterTest.cs
@@ -13,9 +13,23 @@ namespace Tests
         }
 
         [Fact]
+        public void InversionOf7Mod26Returns15()
+        {
+            ModularInverter.GetInversionNaive(7, 26, out int inverse);
+            Assert.Equal(15, inverse);
+        }
+
+        [Fact]
         public void InversionOf4Mod26DoesNotExist()
         {
             Assert.False(ModularInverter.GetInversionNaive(4, 26, out int _));
+        }
+
+        [Fact]
+        public void ReturnsZeroWhenInverseDoesNotExist()
+        {
+            ModularInverter.GetInversionNaive(4, 26, out int inverse);
+            Assert.Equal(0, inverse);
         }
     }
 }
