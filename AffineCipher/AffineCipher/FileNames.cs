e731d6b8817ca1a7d48f72099b30cfbdf3403273
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-09 22:24:41 +0200

    Add file name consts

---
+++ AffineCipher/AffineCipher/FileNames.cs
@@ -0,0 +1,15 @@
+﻿namespace AffineCipher
+{
+    public class PathConstants
+    {
+        public const string SourceFile = "plain.txt";
+
+        public const string KeyFile = "key.txt";
+
+        public const string EncryptedFile = "crypto.txt";
+
+        public const string DecryptedFile = "decrypt.txt";
+
+        public const string ExtraFile = "extra.txt";
+    }
+}
