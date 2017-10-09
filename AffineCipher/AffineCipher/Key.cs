33548d5a5d79ac3d477ef347b37cd9f019afcbb2
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-10 01:49:18 +0200

    Add key class

---
+++ AffineCipher/AffineCipher/Key.cs
@@ -0,0 +1,15 @@
+﻿namespace AffineCipher
+{
+    public class Key
+    {
+        public int Multiplier;
+
+        public int Addend;
+
+        public Key(int multiplier, int addend)
+        {
+            Multiplier = multiplier;
+            Addend = addend;
+        }
+    }
+}
