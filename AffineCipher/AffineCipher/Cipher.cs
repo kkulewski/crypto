255515f4bd050f9f050f231047680d6d59dc636c
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-09 23:03:41 +0200

    Add caesar decryption

--- AffineCipher/AffineCipher/Cipher.cs
+++ AffineCipher/AffineCipher/Cipher.cs
@@ -22,5 +22,27 @@
 
             return new string(chars);
         }
+
+        public static string DecryptCaesar(string input, int key)
+        {
+            var alphabetSize = 'z' - 'a' + 1;
+            var chars = input.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] >= 'a' && chars[i] <= 'z')
+                {
+                    // add alphabetSize to ensure mod on non-negative integers
+                    chars[i] = (char)((chars[i] - 'a' - key + alphabetSize) % alphabetSize + 'a');
+                }
+
+                if (chars[i] >= 'A' && chars[i] <= 'Z')
+                {
+                    chars[i] = (char)((chars[i] - 'A' - key + alphabetSize) % alphabetSize + 'A');
+                }
+            }
+
+            return new string(chars);
+        }
     }
 }
