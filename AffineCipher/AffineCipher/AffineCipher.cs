d4963d474e79bdfac420dedd1dcc2809956f8bfc
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-10 00:44:32 +0200

    Add affine cipher

---
+++ AffineCipher/AffineCipher/AffineCipher.cs
@@ -0,0 +1,20 @@
+﻿namespace AffineCipher
+{
+    class AffineCipher : Cipher
+    {
+        public override string Encrypt(string input, int keyAddend, int keyMultiplier = 1)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string Decrypt(string input, int keyAddend, int keyMultiplier = 1)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int RunCryptoanalysisWithPlain(string plain, string encrypted)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
