f3e95c550dea410f7e403ee0bbac85a937d2bbd7
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-10 22:48:23 +0200

    Update references to modulo class

--- AffineCipher/Tests/ModularInverterTests.cs
+++ AffineCipher/Tests/ModularInverterTests.cs
@@ -9,26 +9,26 @@ namespace Test
         [Fact]
         public void InversionOf7Mod26Exists()
         {
-            Assert.True(ModuloHelpers.GetInversionNaive(7, 26, out int _));
+            Assert.True(Modulo.GetInversionNaive(7, 26, out int _));
         }
 
         [Fact]
         public void InversionOf7Mod26Returns15()
         {
-            ModuloHelpers.GetInversionNaive(7, 26, out int inverse);
+            Modulo.GetInversionNaive(7, 26, out int inverse);
             Assert.Equal(15, inverse);
         }
 
         [Fact]
         public void InversionOf4Mod26DoesNotExist()
         {
-            Assert.False(ModuloHelpers.GetInversionNaive(4, 26, out int _));
+            Assert.False(Modulo.GetInversionNaive(4, 26, out int _));
         }
 
         [Fact]
         public void ReturnsZeroWhenInverseDoesNotExist()
         {
-            ModuloHelpers.GetInversionNaive(4, 26, out int inverse);
+            Modulo.GetInversionNaive(4, 26, out int inverse);
             Assert.Equal(0, inverse);
         }
     }
