7c0d6dcc8daf28f05e4e5892dd06426c78c6b9ad
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-10 02:53:11 +0200

    Add operation result class

---
+++ AffineCipher/AffineCipher/OperationResult.cs
@@ -0,0 +1,15 @@
+﻿namespace AffineCipher
+{
+    public class OperationResult
+    {
+        public bool Success;
+
+        public string Message;
+
+        public OperationResult(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+}
