78f644b49db4b4f04769bf5bdb6da190ffe10fd6
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-11 21:44:13 +0200

    Add affine cryptoanalysis tests

--- AffineCipher/Tests/AffineCipherTests.cs
+++ AffineCipher/Tests/AffineCipherTests.cs
@@ -1,4 +1,3 @@
-﻿using System;
 using AffineCipher;
 using AffineCipher.Ciphers;
 using Xunit;
@@ -10,20 +9,20 @@ namespace Test
         private readonly Cipher _cipher = new AffineCipher.Ciphers.AffineCipher();
 
         [Fact]
-        public void EncryptWithKey17and3()
+        public void EncryptWithKey17And17()
         {
             var key = new Key(17, 17);
             var input = "test";
             var output = _cipher.Encrypt(input, key);
             
-            Assert.Equal("sxbs", output);
+            Assert.Equal("chlc", output);
         }
 
         [Fact]
-        public void DecryptWithKey17and3()
+        public void DecryptWithKey17And17()
         {
             var key = new Key(17, 17);
-            var input = "sxbs";
+            var input = "chlc";
             var output = _cipher.Decrypt(input, key);
 
             Assert.Equal("test", output);
@@ -32,8 +31,8 @@ namespace Test
         [Fact]
         public void TwoWayEncryption()
         {
-            var key = new Key(17, 17);
-            var input = "test";
+            var key = new Key(17, 13);
+            var input = "test abcdefghijklmnopqrstuwvxyz 123 . HElloO";
             var encrypted = _cipher.Encrypt(input, key);
             var decrypted = _cipher.Decrypt(encrypted, key);
             
@@ -54,10 +53,39 @@ namespace Test
         public void RunCryptoanalysisWithPlainReturnsCorrectKeyAddend()
         {
             var key = new Key(17, 17);
-            var input = "test";
+            var input = "test asdfghjkl QWERTY zxcv";
             var output = _cipher.Encrypt(input, key);
             
             Assert.Equal(key.Addend, _cipher.RunCryptoanalysisWithPlain(input, output).Addend);
+        }
+
+        [Fact]
+        public void EncryptDecryptAndCryptoanalysisReturnsCorrectKey()
+        {
+            var key = new Key(5, 13);
+            var input = "qwertyuiop";
+            var encrypted = _cipher.Encrypt(input, key);
+            var decrypted = _cipher.Decrypt(encrypted, key);
+            var foundKey = _cipher.RunCryptoanalysisWithPlain(decrypted, encrypted);
+            
+            Assert.Equal(input, decrypted);
+            Assert.Equal(key.Multiplier, foundKey.Multiplier);
+            Assert.Equal(key.Addend, foundKey.Addend);
+
+        }
+
+        [Fact]
+        public void EncryptDecryptCryptoanalysisReturnsCorrectKey2()
+        {
+            var key = new Key(11, 19);
+            var input = "abcXYZ";
+            var encrypted = _cipher.Encrypt(input, key);
+            var decrypted = _cipher.Decrypt(encrypted, key);
+            var foundKey = _cipher.RunCryptoanalysisWithPlain(decrypted, encrypted);
+
+            Assert.Equal(input, decrypted);
+            Assert.Equal(key.Multiplier, foundKey.Multiplier);
+            Assert.Equal(key.Addend, foundKey.Addend);
         }
     }
 }
