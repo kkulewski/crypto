ce4cce385025a1f4dbbf77556e2c582a14444f4b
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-11 02:07:10 +0200

    Add affine cipher tests

---
+++ AffineCipher/Tests/AffineCipherTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using AffineCipher;
+using AffineCipher.Ciphers;
+using Xunit;
+
+namespace Test
+{
+    public class AffineCipherTests
+    {
+        private readonly Cipher _cipher = new AffineCipher.Ciphers.AffineCipher();
+
+        [Fact]
+        public void EncryptWithKey17and3()
+        {
+            var key = new Key(17, 17);
+            var input = "test";
+            var output = _cipher.Encrypt(input, key);
+            
+            Assert.Equal("sxbs", output);
+        }
+
+        [Fact]
+        public void DecryptWithKey17and3()
+        {
+            var key = new Key(17, 17);
+            var input = "sxbs";
+            var output = _cipher.Decrypt(input, key);
+
+            Assert.Equal("test", output);
+        }
+
+        [Fact]
+        public void TwoWayEncryption()
+        {
+            var key = new Key(17, 17);
+            var input = "test";
+            var encrypted = _cipher.Encrypt(input, key);
+            var decrypted = _cipher.Decrypt(encrypted, key);
+            
+            Assert.Equal(input, decrypted);
+        }
+
+        [Fact]
+        public void RunCryptoanalysisWithPlainReturnsCorrectKeyMultiplier()
+        {
+            var key = new Key(17, 17);
+            var input = "test";
+            var output = _cipher.Encrypt(input, key);
+
+            Assert.Equal(key.Multiplier, _cipher.RunCryptoanalysisWithPlain(input, output).Multiplier);
+        }
+
+        [Fact]
+        public void RunCryptoanalysisWithPlainReturnsCorrectKeyAddend()
+        {
+            var key = new Key(17, 17);
+            var input = "test";
+            var output = _cipher.Encrypt(input, key);
+            
+            Assert.Equal(key.Addend, _cipher.RunCryptoanalysisWithPlain(input, output).Addend);
+        }
+    }
+}
