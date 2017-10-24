1852935c5e357f671ba9c4a797581995ecd81fcb
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-24 23:17:37 +0200

    Add file name consts

---
+++ OTP/OTP/FileNames.cs
@@ -0,0 +1,16 @@
+﻿namespace OTP
+{
+    public static class FileNames
+    {
+        public const string OriginalText = "orig.txt";
+
+        public const string PreparedText = "plain.txt";
+
+        public const string Key = "key.txt";
+
+        public const string EncryptedText = "crypto.txt";
+
+        public const string DecryptedText = "decrypt.txt";
+
+    }
+}
