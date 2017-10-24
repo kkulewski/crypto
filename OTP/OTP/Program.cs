f3f6bd776691ace1f10e6ff658b3a6540ac93ed7
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-25 00:15:59 +0200

    Add prepare text impl

--- OTP/OTP/Program.cs
+++ OTP/OTP/Program.cs
@@ -22,7 +22,7 @@ namespace OTP
             switch (args[0])
             {
                 case "-p":
-                    //PrepareText();
+                    PrepareText();
                     break;
 
                 case "-e":
@@ -37,6 +37,38 @@ namespace OTP
                     Console.WriteLine("Wrong action parameter!" + parameterHelp);
                     break;
             }
+        }
+
+        public static void PrepareText()
+        {
+            int lineLength = 16 - 1;
+
+            var input = File.ReadAllText(FileNames.OriginalText);
+            input = input.ToLower().RemoveForbiddenCharacters();
+
+            var preparedText = new StringBuilder();
+            while (input.Length > lineLength)
+            {
+                var line = input.Substring(0, lineLength);
+                input = input.Substring(lineLength, input.Length - lineLength);
+                preparedText.AppendLine(line);
+            }
+
+            File.WriteAllText(FileNames.PreparedText, preparedText.ToString());
+        }
+
+        public static string RemoveForbiddenCharacters(this string input)
+        {
+            var forbiddenChars =
+                "! @ # $ % ^ & * ( ) _ + 1 2 3 4 5 6 7 8 9 0 - = [ ] { } ; ' : , . < > / ? \\ |"
+                    .Split(' ');
+
+            foreach (var s in forbiddenChars)
+            {
+                input = input.Replace(s, "");
+            }
+
+            return input;
         }
     }
 }
