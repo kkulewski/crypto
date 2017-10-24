5c2b30d0538ad1cc6097fb55cc2975f82a0192c1
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-24 23:17:03 +0200

    Add OTP solution

---
+++ OTP/OTP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
