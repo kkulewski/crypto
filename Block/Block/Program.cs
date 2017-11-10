c52f56d9cde47d68b8ba218fdd1b1000ee974cfe
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-10 20:17:18 +0100

    Change pixels

--- Block/Block/Program.cs
+++ Block/Block/Program.cs
@@ -14,6 +14,22 @@ namespace Block
             var img = LoadImage(args[0]);
             Console.WriteLine("Width : " + img.Width);
             Console.WriteLine("Height: " + img.Height);
+
+            var key = new[] {0, 50, 100, 150};
+
+            for (var i = 0; i < img.Height; i++)
+            {
+                for (var j = 0; j < img.Width; j++)
+                {
+                    var color = img.GetPixel(j, i);
+                    var offset = key[(j + i) % 4];
+                    var newColor = Color.FromArgb(color.A, (color.R + offset) % 255, (color.G + offset) % 255, (color.B + offset) % 255);
+                    img.SetPixel(j, i, newColor);
+                    //Console.WriteLine(color.R.ToString(), color.G.ToString(), color.B.ToString());
+                }
+            }
+
+            img.Save(args[1]);
         }
 
         public static Bitmap LoadImage(string fileName)
