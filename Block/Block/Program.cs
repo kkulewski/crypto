a63a2b2a50a9e64355bb6b01cb1b2ab3146fbc92
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-10 21:10:00 +0100

    Remove flip4 encryption

--- Block/Block/Program.cs
+++ Block/Block/Program.cs
@@ -39,31 +39,6 @@ namespace Block
             img.SetPixel(p.X, p.Y, IsPixelBlack(p, img) ? Color.White : Color.Black);
         }
 
-        public static Bitmap Flip4Encryption(Bitmap sourceImage, bool[] key)
-        {
-            var img = (Bitmap) sourceImage.Clone();
-
-            for (var i = 0; i < img.Width / 2; i++)
-            {
-                for (var j = 0; j < img.Height / 2; j++)
-                {
-                    var p1 = new Point(i * 2, j * 2);
-                    var p2 = new Point(i * 2, j * 2 + 1);
-                    var p3 = new Point(i * 2 + 1, j * 2);
-                    var p4 = new Point(i * 2 + 1, j * 2 + 1);
-
-                    var points = new Point[] { p1, p2, p3, p4 };
-                    foreach (var p in points)
-                    {
-                        if (key[i % 4])
-                            FlipColor(p, img);
-                    }
-                }
-            }
-
-            return img;
-        }
-
         public static Bitmap Flip16Encryption(Bitmap sourceImage, bool[] key)
         {
             var image = (Bitmap)sourceImage.Clone();
