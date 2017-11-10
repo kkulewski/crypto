6c7e4962b3390fdbebd965f9dfd52010032115d4
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-10 20:55:47 +0100

    Add flip4 encryption

--- Block/Block/Program.cs
+++ Block/Block/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
 
 namespace Block
@@ -12,29 +8,52 @@ namespace Block
         static void Main(string[] args)
         {
             var img = LoadImage(args[0]);
-            Console.WriteLine("Width : " + img.Width);
-            Console.WriteLine("Height: " + img.Height);
 
-            var key = new[] {0, 50, 100, 150};
+            var key = new[] {false, true, true, true};
 
-            for (var i = 0; i < img.Height; i++)
-            {
-                for (var j = 0; j < img.Width; j++)
-                {
-                    var color = img.GetPixel(j, i);
-                    var offset = key[(j + i) % 4];
-                    var newColor = Color.FromArgb(color.A, (color.R + offset) % 255, (color.G + offset) % 255, (color.B + offset) % 255);
-                    img.SetPixel(j, i, newColor);
-                    //Console.WriteLine(color.R.ToString(), color.G.ToString(), color.B.ToString());
-                }
-            }
-
-            img.Save(args[1]);
+            var newImg = Flip4Encryption(key, img);
+            newImg.Save(args[1]);
         }
 
         public static Bitmap LoadImage(string fileName)
         {
             return (Bitmap) Image.FromFile(fileName);
+        }
+
+        public static bool IsPixelBlack(Point p, Bitmap img)
+        {
+            var px = img.GetPixel(p.X, p.Y);
+            return px.R == 0 && px.G == 0 && px.B == 0;
+        }
+
+        public static void FlipColor(Point p, Bitmap img)
+        {
+            img.SetPixel(p.X, p.Y, IsPixelBlack(p, img) ? Color.White : Color.Black);
+        }
+
+        public static Bitmap Flip4Encryption(bool[] key, Bitmap source)
+        {
+            var img = (Bitmap) source.Clone();
+
+            for (var i = 0; i < img.Width / 2; i++)
+            {
+                for (var j = 0; j < img.Height / 2; j++)
+                {
+                    var p1 = new Point(i * 2, j * 2);
+                    var p2 = new Point(i * 2, j * 2 + 1);
+                    var p3 = new Point(i * 2 + 1, j * 2);
+                    var p4 = new Point(i * 2 + 1, j * 2 + 1);
+
+                    var points = new Point[] { p1, p2, p3, p4 };
+                    foreach (var p in points)
+                    {
+                        if (key[i % 4])
+                            FlipColor(p, img);
+                    }
+                }
+            }
+
+            return img;
         }
     }
 }
