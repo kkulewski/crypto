39a4989483d96ededb5a5b2481c965afbce2a84d
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-10 23:28:06 +0100

    Extract image encryption to separate class

---
+++ Block/Block/ImageEncryption.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Block
+{
+    public class ImageEncryption
+    {
+        public readonly int BlockWidth;
+
+        public readonly int BlockHeight;
+
+        public ImageEncryption(int blockWidth, int blockHeight)
+        {
+            BlockWidth = blockWidth;
+            BlockHeight = blockHeight;
+        }
+
+        public Bitmap CbcEncrypt(Bitmap sourceImage, bool[] key)
+        {
+            var image = (Bitmap) sourceImage.Clone();
+            var blockSize = BlockWidth * BlockHeight;
+            EcbEncryptBlock(image, key, 0, 0);
+
+            for (var x = 1; x < image.Width / BlockWidth; x++)
+            {
+                for (var y = 1; y < image.Height / BlockHeight; y++)
+                {
+                    var previousBlockAsKey = GetBlockKey(image, x - 1, y - 1);
+                    var newKey = new bool[blockSize];
+
+                    for (var i = 0; i < blockSize; i++)
+                    {
+                        newKey[i] = previousBlockAsKey[i] ^ key[i];
+                    }
+
+                    EcbEncryptBlock(image, newKey, x, y);
+                }
+            }
+
+            return image;
+        }
+
+        public Bitmap EcbEncrypt(Bitmap sourceImage, bool[] key)
+        {
+            var image = (Bitmap) sourceImage.Clone();
+
+            for (var x = 0; x < image.Width / BlockWidth; x++)
+            {
+                for (var y = 0; y < image.Height / BlockHeight; y++)
+                {
+                    EcbEncryptBlock(image, key, x, y);
+                }
+            }
+
+            return image;
+        }
+
+        private void EcbEncryptBlock(Bitmap image, bool[] key, int x, int y)
+        {
+            var block = GetPixelBlock(x, y).ToArray();
+            for (var px = 0; px < BlockWidth * BlockHeight; px++)
+            {
+                if (key[px])
+                    FlipPixelColor(image, block[px]);
+            }
+        }
+
+        private bool[] GetBlockKey(Bitmap image, int x, int y)
+        {
+            return GetPixelBlock(x, y).Select(px => IsPixelBlack(image, px)).ToArray();
+        }
+
+        private IEnumerable<Point> GetPixelBlock(int x, int y)
+        {
+            var block = new List<Point>();
+            for (var k = 0; k < BlockWidth; k++)
+            {
+                for (var n = 0; n < BlockHeight; n++)
+                {
+                    block.Add(new Point(x * BlockWidth + k, y * BlockHeight + n));
+                }
+            }
+
+            return block;
+        }
+
+        private void FlipPixelColor(Bitmap image, Point px)
+        {
+            var newColor = IsPixelBlack(image, px) ? Color.White : Color.Black;
+            image.SetPixel(px.X, px.Y, newColor);
+        }
+
+        private bool IsPixelBlack(Bitmap image, Point px)
+        {
+            var color = image.GetPixel(px.X, px.Y);
+            return color.R == 0 && color.G == 0 && color.B == 0;
+        }
+    }
+}
--- Block/Block/Program.cs
+++ Block/Block/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -16,12 +15,13 @@ namespace Block
         public const string EcbEncryptedFileName = "ecb_crypto.bmp";
         public const string CbcEncryptedFileName = "cbc_crypto.bmp";
 
-        static void Main(string[] args)
+        static void Main()
         {
+            var encryption = new ImageEncryption(BlockWidth, BlockHeight);
             var img = LoadImage(PlainFileName);
             var key = LoadKey(KeyFileName);
-            EcbEncrypt(img, key).Save(EcbEncryptedFileName);
-            CbcEncrypt(img, key).Save(CbcEncryptedFileName);
+            encryption.EcbEncrypt(img, key).Save(EcbEncryptedFileName);
+            encryption.CbcEncrypt(img, key).Save(CbcEncryptedFileName);
         }
 
         public static bool[] LoadKey(string fileName)
@@ -40,87 +40,6 @@ namespace Block
         public static Bitmap LoadImage(string fileName)
         {
             return (Bitmap) Image.FromFile(fileName);
-        }
-
-        public static Bitmap CbcEncrypt(Bitmap sourceImage, bool[] key)
-        {
-            var image = (Bitmap) sourceImage.Clone();
-            var blockSize = BlockWidth * BlockHeight;
-            EcbEncryptBlock(image, key, 0, 0);
-
-            for (var x = 1; x < image.Width / BlockWidth; x++)
-            {
-                for (var y = 1; y < image.Height / BlockHeight; y++)
-                {
-                    var previousBlockAsKey = GetBlockKey(image, x - 1, y - 1);
-                    var newKey = new bool[blockSize];
-
-                    for (var i = 0; i < blockSize; i++)
-                    {
-                        newKey[i] = previousBlockAsKey[i] ^ key[i];
-                    }
-
-                    EcbEncryptBlock(image, newKey, x, y);
-                }
-            }
-
-            return image;
-        }
-
-        public static Bitmap EcbEncrypt(Bitmap sourceImage, bool[] key)
-        {
-            var image = (Bitmap)sourceImage.Clone();
-
-            for (var x = 0; x < image.Width / BlockWidth; x++)
-            {
-                for (var y = 0; y < image.Height / BlockHeight; y++)
-                {
-                    EcbEncryptBlock(image, key, x, y);
-                }
-            }
-
-            return image;
-        }
-
-        public static void EcbEncryptBlock(Bitmap image, bool[] key, int x, int y)
-        {
-            var block = GetPixelBlock(x, y).ToArray();
-            for (var px = 0; px < BlockWidth * BlockHeight; px++)
-            {
-                if (key[px])
-                    FlipPixelColor(image, block[px]);
-            }
-        }
-
-        public static bool[] GetBlockKey(Bitmap image, int x, int y)
-        {
-            return GetPixelBlock(x, y).Select(px => IsPixelBlack(image, px)).ToArray();
-        }
-
-        public static IEnumerable<Point> GetPixelBlock(int x, int y)
-        {
-            var block = new List<Point>();
-            for (var k = 0; k < BlockWidth; k++)
-            {
-                for (var n = 0; n < BlockHeight; n++)
-                {
-                    block.Add(new Point(x * BlockWidth + k, y * BlockHeight + n));
-                }
-            }
-
-            return block;
-        }
-
-        public static void FlipPixelColor(Bitmap image, Point px)
-        {
-            var newColor = IsPixelBlack(image, px) ? Color.White : Color.Black;
-            image.SetPixel(px.X, px.Y, newColor);
-        }
-
-        public static bool IsPixelBlack(Bitmap image, Point px)
-        {
-            var color = image.GetPixel(px.X, px.Y);
-            return color.R == 0 && color.G == 0 && color.B == 0;
         }
     }
 }
