edadfeb48237afc252695ecd825863415d628a78
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-06 01:42:45 +0100

    Add cmd line args handling

--- Vigenere/Vigenere/Program.cs
+++ Vigenere/Vigenere/Program.cs
@@ -10,6 +10,48 @@ namespace Vigenere
     {
         static void Main(string[] args)
         {
+            const string parameterHelp = " Try:" +
+                                         "\n-p -- prepare text" +
+                                         "\n-e -- encrypt text" +
+                                         "\n-d -- decrypt text" +
+                                         "\n-k -- cryptoanalysis";
+
+            if (args.Length != 1 || args[0] == null)
+            {
+                Console.WriteLine("Missing action parameter!" + parameterHelp);
+                return;
+            }
+
+            try
+            {
+                switch (args[0])
+                {
+                    case "-p":
+                        // prepare text
+                        break;
+
+                    case "-e":
+                        // encrypt
+                        break;
+
+                    case "-d":
+                        // decrypt
+                        break;
+
+                    case "-k":
+                        // cryptoanalysis
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong action parameter!" + parameterHelp);
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
         }
     }
 }
