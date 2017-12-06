f80f8d55351303c6ec5025d8a0d4efceb7459949
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-06 01:42:18 +0100

    Add file name consts

---
+++ Vigenere/Vigenere/FileNames.cs
@@ -0,0 +1,17 @@
+﻿namespace Vigenere
+{
+    public static class FileNames
+    {
+        public const string OriginalText = "orig.txt";
+
+        public const string PreparedText = "plain.txt";
+
+        public const string Key = "key.txt";
+
+        public const string EncryptedText = "crypto.txt";
+
+        public const string DecryptedText = "decrypt.txt";
+
+        public const string CrackedKey = "key-crypto.txt";
+    }
+}
