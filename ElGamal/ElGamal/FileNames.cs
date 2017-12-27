a6d3c0e15558eaf77c45e218a2b34a03bf2c2918
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-27 20:23:31 +0100

    Add file name consts

---
+++ ElGamal/ElGamal/FileNames.cs
@@ -0,0 +1,23 @@
+﻿namespace ElGamal
+{
+    public static class FileNames
+    {
+        public const string Generator = "elgamal.txt";
+
+        public const string PublicKey = "public.txt";
+
+        public const string PrivateKey = "private.txt";
+
+        public const string PlainText = "plain.txt";
+
+        public const string EncryptedText = "crypto.txt";
+
+        public const string DecryptedText = "decrypt.txt";
+
+        public const string MessageText = "message.txt";
+
+        public const string Signature = "signature.txt";
+
+        public const string VerifySignature = "verify.txt";
+    }
+}
